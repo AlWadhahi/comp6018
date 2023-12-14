44204ece8428f1b986493febd89e304311202579
Firas Al Wadhahi <Firas.Wadhahi@rihal.om>
2023-12-14 07:56:14 +0400

    chore: update code format

--- homework2/server.py
+++ homework2/server.py
@@ -3,7 +3,10 @@ import grpc
 import course_registeration_pb2
 import course_registeration_pb2_grpc
 
-class CourseRegistrationServicer(course_registeration_pb2_grpc.CourseRegistrationServicer):
+
+class CourseRegistrationServicer(
+    course_registeration_pb2_grpc.CourseRegistrationServicer
+):
     def __init__(self):
         self.courses = {}  # Dictionary to store courses
         self.student_registrations = {}  # Dictionary to store student registrations
@@ -11,62 +14,65 @@ class CourseRegistrationServicer(course_registeration_pb2_grpc.CourseRegistratio
     def AddCourse(self, request, context):
         if request.course_code in self.courses:
             return course_registeration_pb2.AddCourseResponse(
-                success=False,
-                message="Course already exists."
+                success=False, message="Course already exists."
             )
         else:
             self.courses[request.course_code] = request.course_title
             return course_registeration_pb2.AddCourseResponse(
-                success=True,
-                message="Course added successfully."
+                success=True, message="Course added successfully."
             )
 
     def RegisterStudent(self, request, context):
         if request.course_code not in self.courses:
             return course_registeration_pb2.RegisterStudentResponse(
-                success=False,
-                message="Course not found."
+                success=False, message="Course not found."
             )
         if request.course_code in self.student_registrations:
             if request.student_id in self.student_registrations[request.course_code]:
                 return course_registeration_pb2.RegisterStudentResponse(
-                    success=False,
-                    message="Student already registered in this course."
+                    success=False, message="Student already registered in this course."
                 )
-        
-        self.student_registrations.setdefault(request.course_code, {})[request.student_id] = request.student_name
+
+        self.student_registrations.setdefault(request.course_code, {})[
+            request.student_id
+        ] = request.student_name
         return course_registeration_pb2.RegisterStudentResponse(
-            success=True,
-            message="Student registered successfully."
+            success=True, message="Student registered successfully."
         )
 
     def ListCourses(self, request, context):
-        courses = [course_registeration_pb2.Course(course_code=code, course_title=title) 
-                   for code, title in self.courses.items()]
+        courses = [
+            course_registeration_pb2.Course(course_code=code, course_title=title)
+            for code, title in self.courses.items()
+        ]
         return course_registeration_pb2.ListCoursesResponse(courses=courses)
 
     def ListStudents(self, request, context):
         if request.course_code not in self.courses:
             return course_registeration_pb2.ListStudentsResponse(
-                success=False,
-                message="Course not found."
+                success=False, message="Course not found."
             )
-        students = [course_registeration_pb2.Student(student_id=id_, student_name=name) 
-                    for id_, name in self.student_registrations.get(request.course_code, {}).items()]
+        students = [
+            course_registeration_pb2.Student(student_id=id_, student_name=name)
+            for id_, name in self.student_registrations.get(
+                request.course_code, {}
+            ).items()
+        ]
         return course_registeration_pb2.ListStudentsResponse(
-            students=students,
-            success=True,
-            message="Students listed successfully."
+            students=students, success=True, message="Students listed successfully."
         )
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     course_registeration_pb2_grpc.add_CourseRegistrationServicer_to_server(
-        CourseRegistrationServicer(), server)
-    server.add_insecure_port('[::]:50051')
+        CourseRegistrationServicer(), server
+    )
+    server.add_insecure_port("[::]:50051")
     server.start()
     print("Server started, listening on 50051")
     server.wait_for_termination()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     serve()
--- homework2/student.py
+++ homework2/student.py
@@ -3,10 +3,15 @@ import course_registeration_pb2
 import course_registeration_pb2_grpc
 from tabulate import tabulate
 
+
 def register_student(stub, course_code, student_id, student_name):
-    response = stub.RegisterStudent(course_registeration_pb2.RegisterStudentRequest(
-        course_code=course_code, student_id=student_id, student_name=student_name))
+    response = stub.RegisterStudent(
+        course_registeration_pb2.RegisterStudentRequest(
+            course_code=course_code, student_id=student_id, student_name=student_name
+        )
+    )
     print(response.message)
+
 
 def list_courses(stub):
     response = stub.ListCourses(course_registeration_pb2.ListCoursesRequest())
@@ -18,15 +23,16 @@ def list_courses(stub):
     )
     print()  # add new line to separate next prompt
 
+
 def run():
-
-     with grpc.insecure_channel('localhost:50051') as channel:
-
+    with grpc.insecure_channel("localhost:50051") as channel:
         choice = ""
 
         while True:
-            choice = input("\n[1] Enroll in a course\n[2] List courses\n[3] Exit\nSelect Option:")
-            
+            choice = input(
+                "\n[1] Enroll in a course\n[2] List courses\n[3] Exit\nSelect Option:"
+            )
+
             if choice == "1":
                 stub = course_registeration_pb2_grpc.CourseRegistrationStub(channel)
                 course_code = input("Enter course code:")
@@ -41,5 +47,6 @@ def run():
             elif choice == "3":
                 exit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
